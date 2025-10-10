ee96f9f38cdef935712968c44b45663849eee38e
ashutosh <dikkatwarashutosh@gmail.com>
2025-10-10 19:54:54 +0530

    added map current location

--- map.js
+++ map.js
@@ -1,17 +1,74 @@
-let next_btn = document.getElementById("next_btn");
+let check_btn = document.getElementById("check_btn");
+
+
+
+async function getWeather(lat, lng) {
+  const url = `https://api.weatherapi.com/v1/current.json?key=d829f60291da4cfc9ea125607250410&q=${lat},${lng}`;
+
+  try {
+    const response = await fetch(url);
+    const data = await response.json();
+
+    // Store location and temperature only
+    const weatherData = {
+      location: data.location.name,
+      temperature: data.current.temp_c,
+      condition: "https:" + data.current.condition.icon,
+      cloud_condition: data.current.condition.text,
+      humidity: data.current.humidity,
+      cloud: data.current.cloud
+    };
+
+
+    if (weatherData) {
+      document.getElementById("location").innerText = "📍" + weatherData.location;
+      document.getElementById("temp_C").innerText = weatherData.temperature + "°C";
+      document.getElementById("icon").src = weatherData.condition;
+      document.getElementById("broken_cloud").innerText = weatherData.cloud_condition;
+      document.getElementById("humidity_label").innerText = "Humidity:" + weatherData.humidity;
+      document.getElementById("cloudy_label").innerText = "Cloud:" + weatherData.cloud;
+    }
+    else {
+      document.body.innerHTML = "<h3>No data found. Please go back and search again.</h3>";
+    }
+  }
+  catch (error) {
+    console.error("Error fetching weather data:", error);
+    alert("Unable to fetch weather data.");
+  }
+}
+
+if (navigator.geolocation) {
+
+  navigator.geolocation.getCurrentPosition(
+    (position) => {
+      const lat = position.coords.latitude;
+      const lng = position.coords.longitude;
+
+      getWeather(lat, lng);
+    },
+    () => {
+      alert("Location access denied. You can still click on map to choose location.")
+    }
+  );
+} else {
+  alert("Geolocation not supported in this browser.");
+}
+
 
 // Create map
 var map = L.map('map', {
-      scrollWheelZoom: true, // enable zooming with scroll
-      dragging: true         // enable dragging
-    }).setView([28.6139, 77.2090], 10);
+  scrollWheelZoom: true, // enable zooming with scroll
+  dragging: true         // enable dragging
+}).setView([lat, lat], 10);
 
 // Add OpenStreetMap tiles
 L.tileLayer('https://{s}.tile.openstreetmap.org/{z}/{x}/{y}.png', {
   attribution: '© OpenStreetMap contributors'
 }).addTo(map);
 
-var marker = L.marker([28.6139, 77.2090]).addTo(map);
+var marker = L.marker([lat, lat]).addTo(map);
+
 
 // On map click
 map.on('click', function (e) {
@@ -23,35 +80,8 @@ map.on('click', function (e) {
   document.getElementById("output").innerText =
     "Latitude: " + lat + " | Longitude: " + lng;
 
-  next_btn.addEventListener("click", async () => {
-
-    const url = `https://api.weatherapi.com/v1/current.json?key=d829f60291da4cfc9ea125607250410&q=${lat},${lng}`;
-
-    try {
-      const response = await fetch(url);
-      const data = await response.json();
-
-      // Store location and temperature only
-      const weatherData = {
-        location: data.location.name,
-        temperature: data.current.temp_c,
-        condition: "https:" + data.current.condition.icon,
-        cloud_condition: data.current.condition.text,
-        humidity: data.current.humidity,
-        cloud: data.current.cloud
-      };
-
-      // Save data for next.html
-      localStorage.setItem("weatherData", JSON.stringify(weatherData));
-
-      // Redirect to next page
-      window.location.href = "next.html";
-    }
-    catch (error) {
-      console.error("Error fetching weather data:", error);
-      alert("Unable to fetch weather data.");
-    }
-
-  });
+  check_btn.onclick = () => {
+    getWeather(lat, lng);
+  };
 
 });
